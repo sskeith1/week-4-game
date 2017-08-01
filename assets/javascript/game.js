072954f53e6fc5e295aaff949aac0aac38b51950
sskeith1 <skeith1@babson.edu>
2017-07-31 21:50:20 -0400

    DONEDONEDONEDONEDONE

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
 	var pink = 0;
 	var red = 0;
 
-	function jemrand()	{
+	function jemRand()	{
 		blue = Math.round (Math.random() * (12 - 1) +1);
 		green = Math.round (Math.random() * (12 - 1) +1);
 		pink = Math.round (Math.random() * (12 - 1) +1);
@@ -18,14 +18,14 @@ $(document).ready(function() {
 	}
 
 //Targets (The random number the player is trying to hit)
-	var targetrand = 0;
+	var target = 0;
 
-	function target () {
-		targetrand = Math.round (Math.random() * (120 - 19) + 19);
+	function targetRand () {
+		target = Math.round (Math.random() * (120 - 19) + 19);
 	};
 
 
-//reset all
+//Reset all
 	function reset() {
 		blue = 0;
 		green = 0;
@@ -33,23 +33,113 @@ $(document).ready(function() {
 		red = 0;
 
 		score = 0;
-		targetrand = 0;
-	}
+		target = 0;
+	};
 
-//update
+//Update
 	function update() {
 		$("#win").html("Wins: " + wins)
 		$("#loss").html("Losses: " + losses)
-		$(".guessbox").html("" + targetrand)
-		$(".numbox").html("Your Total score is: " + score)
+		$("#guessbox").html("" + target)
+		$("#numbox").html("Your Total score is: " + score)
 	}
 
+//Winning/Losing
+	//Hint with styling
+		function almost(){
+			if (		score + blue == target || score + green == target||
+						score + pink == target || score + red == target) {
+				document.getElementById("numbox").style.background = "green";
+			} 
+			else if (	score + blue > target || score + green > target||
+						score + pink > target|| score + red > target) {
+				document.getElementById("numbox").style.background = "red";
+			}};
 
-//Game
-	update();
+	//Winning protocol
+		function winner () {
+			alert ("You are a winner!");
+			wins++;
+			update();
+			reset();
 
-	$("blue").on("click", function(){
-		alert("hi");
+		};
+	//Losing protocol
+		function loser () {
+			alert ("You are a loser!");
+			losses++;
+			update();
+			reset();
+		};
+
+	//Initiate Winning/Losing protocol
+		function result() {
+			if (score > 0 && score == target) {
+			winner();
+			gameStart();
+		} else if (score > 0 && score > target) {
+
+			loser();
+			gameStart();
+		}};
+	
+
+
+
+
+
+	
+	
+	
+//@Game Start
+	function gameStart () {
+		reset();
+		jemRand();
+		targetRand();
+		update();
+		document.getElementById("numbox").style.background = "white";
+	};
+
+//========================================================================================
+//========================================================================================
+
+//Game Play
+gameStart();
+
+	$("#blue").on("click", function(){
+		score = score + blue;
+		update();
+		console.log("blue: " + blue);
+		almost();
+		result();
 	});
-console.log()
+
+
+	$("#green").on("click", function(){
+		score = score + green;
+		update();
+		console.log("green: " + green);
+		almost();
+		result();
+	});
+
+
+	$("#pink").on("click", function(){
+		score = score + pink;
+		update();
+		console.log("pink: " + pink);
+		almost();
+		result();
+	});
+
+
+	$("#red").on("click", function(){
+		score = score + red;
+		update();
+		console.log("red: " + red);
+		almost();
+		result();
+	});
+	
+
 });
